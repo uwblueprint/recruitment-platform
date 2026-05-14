387cca4d33a4d279988e5bdae1a6e9e671003cfe
Maggie Chen <mxc.maggiechen@gmail.com>
2026-05-14 18:48:37 -0400

    fix: run linter

--- backend/typescript/types/adminComment.ts
+++ backend/typescript/types/adminComment.ts
@@ -1,16 +1,15 @@
 export type AdminCommentDTO = {
-    id: string;
-    userId: number;
-    applicantRecordId: string;
-    comment: string;
-    createdAt: string;
-    updatedAt: string;
-  };
-  
-  export type CreateAdminCommentDTO = Omit<
-    AdminCommentDTO,
-    "id" | "createdAt" | "updatedAt"
-  >;
-  
-  export type UpdateAdminCommentDTO = Pick<AdminCommentDTO, "comment">;
-  
+  id: string;
+  userId: number;
+  applicantRecordId: string;
+  comment: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type CreateAdminCommentDTO = Omit<
+  AdminCommentDTO,
+  "id" | "createdAt" | "updatedAt"
+>;
+
+export type UpdateAdminCommentDTO = Pick<AdminCommentDTO, "comment">;
