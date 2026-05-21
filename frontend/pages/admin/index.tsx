ac6635086f3bb4567c7316afa24cc6d7fe14f60e
Maggie Chen <mxc.maggiechen@gmail.com>
2026-05-21 19:46:48 -0400

    add: add generic page for admin landing

---
+++ frontend/pages/admin/index.tsx
@@ -0,0 +1,17 @@
+import { ProtectedRoute } from "@/components/contexts/ProtectedRoute";
+import { ReactElement } from "react";
+import { NextPageWithLayout } from "../_app";
+
+const AdminPage: NextPageWithLayout = () => {
+  return (
+    <div className="flex h-screen flex-col bg-white">
+      <main className="flex min-h-0 flex-1 flex-col overflow-hidden" />
+    </div>
+  );
+};
+
+AdminPage.getLayout = (page: ReactElement) => (
+  <ProtectedRoute allowedRoles={["Admin", "User"]}>{page}</ProtectedRoute>
+);
+
+export default AdminPage;
