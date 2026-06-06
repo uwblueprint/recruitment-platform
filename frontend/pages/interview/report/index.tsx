bbb5590fe94bebfd24f50ec23f1fb071c8b2c834
Jocelyn Xu <joce.xu06@gmail.com>
2026-06-06 17:32:32 -0400

    format and edit fonts/colours

--- frontend/pages/interview/report/index.tsx
+++ frontend/pages/interview/report/index.tsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
-import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from "@mui/material";
+import {
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from "@mui/material";
 import { Typography } from "@mui/material";
 import { InterviewConflict } from "@/graphql/typeUtils";
 import {
@@ -10,69 +15,132 @@ import {
 import { NextPageWithLayout } from "../../_app";
 
 const InterviewReportPage: NextPageWithLayout = () => {
-  const [selectedConflict, setSelectedConflict] = useState<InterviewConflict | null>(null);
+  const [selectedConflict, setSelectedConflict] =
+    useState<InterviewConflict | null>(null);
+  const radioColor = "#2E3A59";
+
   return (
     <div className="relative flex h-full flex-col overflow-hidden bg-white">
       <div className="flex h-full flex-col overflow-y-auto py-8 pl-9 pr-44">
-      <div className="flex flex-col gap-4">
-        <div>
-          <h4 className="font-poppins font-semibold text-neutral-800 mb-0">Please select the issue you'd like to report</h4>
-          <Typography variant="body2" color="error">Note: Changes cannot be undone</Typography>
+        <div className="flex flex-col gap-4">
+          <div>
+            <h4 className="font-poppins font-semibold text-neutral-800 mb-0">
+              Please select the issue you'd like to report
+            </h4>
+            <Typography
+              className="!font-source"
+              variant="body2"
+              sx={{ color: "#E55F5F" }}
+            >
+              Note: Changes cannot be undone
+            </Typography>
+          </div>
+          <FormControl fullWidth>
+            <div className="border border-neutral-200 rounded-lg w-full px-7">
+              <RadioGroup
+                value={selectedConflict ?? ""}
+                onChange={(e) =>
+                  setSelectedConflict(e.target.value as InterviewConflict)
+                }
+              >
+                <FormControlLabel
+                  value={InterviewConflict.ApplicantConflict}
+                  sx={{ gap: 2 }}
+                  control={
+                    <Radio
+                      sx={{
+                        color: radioColor,
+                        "&.Mui-checked": {
+                          color: radioColor,
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <div className="py-5">
+                      <p className="font-poppins font-semibold mb-2">
+                        Conflict with Applicant{" "}
+                      </p>
+                      <p className="text-sm font-source">
+                        Conflict of interest with the applicant
+                      </p>
+                    </div>
+                  }
+                />
+                <FormControlLabel
+                  value={InterviewConflict.ApplicantNoResponse}
+                  sx={{ gap: 2 }}
+                  control={
+                    <Radio
+                      sx={{
+                        color: radioColor,
+                        "&.Mui-checked": {
+                          color: radioColor,
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <div className="py-5">
+                      <p className="font-poppins font-semibold mb-2">
+                        Haven't heard back from applicant
+                      </p>
+                      <p className="text-sm font-source">
+                        Applicant has not replied to interview invite
+                      </p>
+                    </div>
+                  }
+                />
+                <FormControlLabel
+                  value={InterviewConflict.PartnerNoResponse}
+                  sx={{ gap: 2 }}
+                  control={
+                    <Radio
+                      sx={{
+                        color: radioColor,
+                        "&.Mui-checked": {
+                          color: radioColor,
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <div className="py-5">
+                      <p className="font-poppins font-semibold mb-2">
+                        Haven't heard back from interview partner
+                      </p>
+                      <p className="text-sm font-source">
+                        Interviewer partner has not responded to messages
+                      </p>
+                    </div>
+                  }
+                />
+                <FormControlLabel
+                  value={InterviewConflict.CannotAttend}
+                  sx={{ gap: 2 }}
+                  control={
+                    <Radio
+                      sx={{
+                        color: radioColor,
+                        "&.Mui-checked": {
+                          color: radioColor,
+                        },
+                      }}
+                    />
+                  }
+                  label={
+                    <div className="py-5">
+                      <p className="font-poppins font-semibold mb-2">
+                        Cannot make interview
+                      </p>
+                      <p className="text-sm font-source">Description here</p>
+                    </div>
+                  }
+                />
+              </RadioGroup>
+            </div>
+          </FormControl>
         </div>
-        <FormControl fullWidth>
-        <div className="border border-neutral-200 rounded-lg w-full px-7">
-          <RadioGroup
-          value={selectedConflict ?? ""}
-          onChange={(e) => setSelectedConflict(e.target.value as InterviewConflict)}
-          >
-            <FormControlLabel 
-              value={InterviewConflict.ApplicantConflict} 
-              sx={{ gap: 2}}
-              control={<Radio color="primary" />}
-              label={
-                <div className="py-5">
-                  <p className="font-semibold mb-2">Conflict with Applicant </p>
-                  <p className="text-sm text-neutral-500">Conflict of interest with the applicant</p>
-                </div>
-              }
-            />
-            <FormControlLabel
-              value={InterviewConflict.ApplicantNoResponse}
-              sx={{ gap: 2}}
-              control={<Radio color="primary" />}
-              label={
-                <div className="py-5">
-                  <p className="font-semibold mb-2">Haven't heard back from applicant</p>
-                  <p className="text-sm text-neutral-500">Applicant has not replied to interview invite</p>
-              </div>
-              }
-            />
-            <FormControlLabel
-              value={InterviewConflict.PartnerNoResponse}
-              sx={{ gap: 2}}
-              control={<Radio color="primary" />}
-              label={
-                <div className="py-5">
-                  <p className="font-semibold mb-2">Haven't heard back from interview partner</p>
-                  <p className="text-sm text-neutral-500">Interviewer partner has not responded to messages</p>
-                </div>
-              }
-            />
-            <FormControlLabel
-              value={InterviewConflict.CannotAttend}
-              sx={{ gap: 2}}
-              control={<Radio color="primary" />}
-              label={
-                <div className="py-5">
-                  <p className="font-semibold mb-2">Cannot make interview</p>
-                  <p className="text-sm text-neutral-500">Description here</p>
-                </div>
-              }
-            />
-          </RadioGroup>
-        </div>
-      </FormControl>
-      </div>
       </div>
     </div>
   );
