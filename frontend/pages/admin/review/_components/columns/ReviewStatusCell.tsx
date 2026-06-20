237bb24234455dcc46c8ba76414f492424a31635
Owen <owenchenyp@gmail.com>
2026-06-20 18:39:39 -0400

    Added chip options for the Interviewed, Offered, and Rejected statuses

--- frontend/pages/admin/review/_components/columns/ReviewStatusCell.tsx
+++ frontend/pages/admin/review/_components/columns/ReviewStatusCell.tsx
@@ -19,9 +19,24 @@ const APPLICATION_STATUS_OPTIONS = [
     className: "bg-green-200 text-black",
   },
   {
+    value: ApplicationStatus.Interviewed,
+    label: "Interviewed",
+    className: "bg-blue-200 text-black",
+  },
+  {
+    value: ApplicationStatus.Offered,
+    label: "Offered",
+    className: "bg-teal-200 text-black",
+  },
+  {
     value: ApplicationStatus.Selected,
     label: "Selected",
     className: "bg-purple-200 text-black",
+  },
+  {
+    value: ApplicationStatus.Rejected,
+    label: "Rejected",
+    className: "bg-red-200 text-black",
   },
 ] as const;
 
